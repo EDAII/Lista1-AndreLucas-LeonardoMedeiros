d2815dd80d26470a7f43293c0ff626cc1bfe4732
Andre Lucas <andrelucax@gmail.com>
2019-03-25 11:16:04 -0300

    Implementation of sequential search and fixing binary search
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>
    Co-authored-by: André Lucas de Sousa Pinto <andrelucax@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <time.h>
-#include <array>
+#include <vector>
 
 using namespace std;
 
-#define NOT_FOUND -1;
+#define NOT_FOUND -404; // Easter Egg
 
-int binarySearch(int * v, int left, int right, int x);
-int seqSearch();
-int sentinelSeqSearch();
-int indexSeqSearch();
+int binarySearch(vector<int> * v, int x);
+int seqSearch(vector<int> * v, int x);
+// int sentinelSeqSearch();
+// int indexSeqSearch();
 
 int main(){
 
-
+  vector<int> v = {1,2,3,4,5};
+  int y = binarySearch(&v, 4);
+  cout << y << endl;
 
   return 0;
 }
 
-int binarySearch(int * v, int left, int right, int x){
-  int mid = (right+left)/2;
-  while(left<right){
-    if(v[mid]==x) return mid;
-    if(v[mid]>x) mid=left;
-    else mid=right;
+int binarySearch(vector<int> * v, int x){
+  int left = 0;
+  int right = v->size()-1;
+  while(left <= right){
+    int mid = (right+left)/2;
+    if(v->at(mid) == x) return mid;
+    if(v->at(mid) > x) right = mid - 1;
+    else left = mid + 1;
+  }
+  return NOT_FOUND;
+}
+
+int seqSearch(vector<int> * v, int x){
+  for (size_t i = 0; i < v->size(); i++) {
+    if (v->at(i) == x){
+      return i;
+    }
   }
   return NOT_FOUND;
 }
